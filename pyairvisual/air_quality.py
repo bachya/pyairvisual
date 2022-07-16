89a8fd872a3a94e6f3a6871cbe9c742b7270600d
Aaron Bach <bachya1208@gmail.com>
2022-07-16 12:43:45 -0600

    Clean up typing (#102)

--- pyairvisual/air_quality.py
+++ pyairvisual/air_quality.py
@@ -1,59 +1,64 @@
 """Define an object to retrieve air quality info."""
-from typing import Callable, Coroutine, Optional, Union
+from __future__ import annotations
+
+from typing import Any, Awaitable, Callable
 
 
 class AirQuality:
     """Define an object to manage air quality API calls."""
 
-    def __init__(self, request: Callable[..., Coroutine]) -> None:
+    def __init__(self, request: Callable[..., Awaitable]) -> None:
         """Iniitialize."""
-        self._request: Callable[..., Coroutine] = request
+        self._request = request
 
     async def _nearest(
         self,
         kind: str,
-        latitude: Optional[Union[float, str]] = None,
-        longitude: Optional[Union[float, str]] = None,
-    ) -> dict:
+        latitude: float | str | None = None,
+        longitude: float | str | None = None,
+    ) -> dict[str, Any]:
         """Return data from nearest city/station (IP or coordinates)."""
-        params: dict = {}
         if latitude and longitude:
-            params.update({"lat": str(latitude), "lon": str(longitude)})
+            params = {"lat": str(latitude), "lon": str(longitude)}
+        else:
+            params = {}
 
-        data: dict = await self._request("get", f"nearest_{kind}", params=params)
+        data = await self._request("get", f"nearest_{kind}", params=params)
         return data["data"]
 
-    async def city(self, city: str, state: str, country: str) -> dict:
+    async def city(self, city: str, state: str, country: str) -> dict[str, Any]:
         """Return data for the specified city."""
-        data: dict = await self._request(
+        data = await self._request(
             "get", "city", params={"city": city, "state": state, "country": country}
         )
         return data["data"]
 
     async def nearest_city(
         self,
-        latitude: Optional[Union[float, str]] = None,
-        longitude: Optional[Union[float, str]] = None,
-    ) -> dict:
+        latitude: float | str | None = None,
+        longitude: float | str | None = None,
+    ) -> dict[str, Any]:
         """Return data from nearest city (IP or coordinates)."""
         return await self._nearest("city", latitude, longitude)
 
     async def nearest_station(
         self,
-        latitude: Optional[Union[float, str]] = None,
-        longitude: Optional[Union[float, str]] = None,
-    ) -> dict:
+        latitude: float | str | None = None,
+        longitude: float | str | None = None,
+    ) -> dict[str, Any]:
         """Return data from nearest station (IP or coordinates)."""
         return await self._nearest("station", latitude, longitude)
 
-    async def ranking(self) -> dict:
+    async def ranking(self) -> dict[str, Any]:
         """Return a sorted array of selected major cities in the world."""
         data = await self._request("get", "city_ranking")
         return data["data"]
 
-    async def station(self, station: str, city: str, state: str, country: str) -> dict:
+    async def station(
+        self, station: str, city: str, state: str, country: str
+    ) -> dict[str, Any]:
         """Return data for the specified station."""
-        data: dict = await self._request(
+        data = await self._request(
             "get",
             "station",
             params={
