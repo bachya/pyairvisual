d540c9617bf7200da78a0da64c54dd65afe4b28e
Aaron Bach <bachya1208@gmail.com>
2022-10-26 15:53:37 -0600

    Clean up tests with proper test fixture usage (#113)
    * Clean up tests with proper test fixture usage
    
    * Docstring

---
+++ tests/node/conftest.py
@@ -0,0 +1,105 @@
+"""Define dynamic test fixtures."""
+import tempfile
+from unittest.mock import Mock, mock_open, patch
+
+import pytest
+
+from tests.common import load_fixture
+
+
+@pytest.fixture(name="error_node_not_found_response", scope="session")
+def error_node_not_found_response_fixture():
+    """Define a fixture for error response data (Node/Pro not found)."""
+    return '"node not found"'
+
+
+@pytest.fixture(name="mock_history_file")
+def mock_history_file_fixture():
+    """Define a fixture to mock a file for history data."""
+    return Mock(name="202003_AirVisual_values.txt")
+
+
+@pytest.fixture(name="mock_measurements_file")
+def mock_measurements_file_fixture(node_measurements_response):
+    """Define a fixture to mock a file for measurement data."""
+    mock = Mock()
+    mock.read.return_value = node_measurements_response.encode()
+    return mock
+
+
+@pytest.fixture(name="mock_open_function", scope="session")
+def mock_open_function_fixture(node_history_samba_response):
+    """Define a fixture to mock the builtin open function."""
+    mop = mock_open(read_data=node_history_samba_response)
+    mop.return_value.__iter__ = lambda self: self
+    mop.return_value.__next__ = lambda self: next(iter(self.readline, ""))
+    return mop
+
+
+@pytest.fixture(name="mock_pysmb_close")
+def mock_pysmb_close_fixture():
+    """Define a fixture to mock the pysmb close method."""
+    return Mock()
+
+
+@pytest.fixture(name="mock_pysmb_connect")
+def mock_pysmb_connect_fixture():
+    """Define a fixture to mock the pysmb connect method."""
+    return Mock()
+
+
+@pytest.fixture(name="mock_pysmb_list_path")
+def mock_pysmb_list_path_fixture(mock_history_file):
+    """Define a fixture to mock the pysmb listPath method."""
+    return Mock(return_value=[mock_history_file])
+
+
+@pytest.fixture(name="mock_pysmb_retrieve_file")
+def mock_pysmb_retrieve_file_fixture():
+    """Define a fixture to mock the pysmb retrieveFile method."""
+    return Mock()
+
+
+@pytest.fixture(name="node_history_samba_response", scope="session")
+def node_history_samba_response_fixture():
+    """Define a fixture for response data containing Node/Pro history info."""
+    return load_fixture("node_history_samba_response.txt")
+
+
+@pytest.fixture(name="node_measurements_file")
+def node_measurements_file_fixture():
+    """Define a fixture for response data containing Node/Pro measurements."""
+    return "node_measurements_samba_dict_response.json"
+
+
+@pytest.fixture(name="node_measurements_response")
+def node_measurements_response_fixture(node_measurements_file):
+    """Define a fixture for a fixture filename containing Node/Pro measurements."""
+    return load_fixture(node_measurements_file)
+
+
+@pytest.fixture(name="setup_samba_connection")
+def setup_samba_connection_fixture(
+    mock_history_file,
+    mock_measurements_file,
+    mock_open_function,
+    mock_pysmb_close,
+    mock_pysmb_connect,
+    mock_pysmb_list_path,
+    mock_pysmb_retrieve_file,
+):
+    """Define a fixture to return a patched Node/Pro Samba connection."""
+    with patch.object(
+        tempfile,
+        "NamedTemporaryFile",
+        side_effect=[mock_measurements_file, mock_history_file],
+    ), patch("smb.SMBConnection.SMBConnection.connect", mock_pysmb_connect), patch(
+        "smb.SMBConnection.SMBConnection.listPath", mock_pysmb_list_path
+    ), patch(
+        "smb.SMBConnection.SMBConnection.retrieveFile", mock_pysmb_retrieve_file
+    ), patch(
+        "smb.SMBConnection.SMBConnection.close", mock_pysmb_close
+    ), patch(
+        "builtins.open", mock_open_function
+    ):
+        yield
---
+++ tests/node/test_requests_responses.py
@@ -0,0 +1,110 @@
+"""Define tests for Node errors."""
+# pylint: disable=unused-argument
+import pytest
+
+from pyairvisual.node import NodeSamba
+
+from tests.common import TEST_NODE_IP_ADDRESS, TEST_NODE_PASSWORD
+
+
+@pytest.mark.asyncio
+async def test_node_by_samba_dict_response(setup_samba_connection):
+    """Test getting a node's info over the local network (via Samba).
+
+    This variant of the test expects a dictionary-esque response from the unit.
+    """
+    async with NodeSamba(TEST_NODE_IP_ADDRESS, TEST_NODE_PASSWORD) as node:
+        measurements = await node.async_get_latest_measurements()
+        history = await node.async_get_history()
+
+    assert len(history["measurements"]) == 7
+    assert measurements["last_measurement_timestamp"] == 1584204767
+    assert measurements["measurements"]["co2"] == "442"
+    assert measurements["measurements"]["humidity"] == "35"
+    assert measurements["measurements"]["pm0_1"] == "3"
+    assert measurements["measurements"]["pm1_0"] == "4"
+    assert measurements["measurements"]["aqi_cn"] == "6"
+    assert measurements["measurements"]["aqi_us"] == "17"
+    assert measurements["measurements"]["pm2_5"] == "4.0"
+    assert measurements["measurements"]["temperature_C"] == "19.3"
+    assert measurements["measurements"]["temperature_F"] == "66.8"
+    assert measurements["measurements"]["voc"] == "-1"
+
+    assert history["trends"] == {
+        "aqi_cn": "decreasing",
+        "aqi_us": "decreasing",
+        "co2": "decreasing",
+        "humidity": "increasing",
+        "pm0_1": "decreasing",
+        "pm1_0": "decreasing",
+        "pm2_5": "decreasing",
+        "voc": "flat",
+    }
+
+
+@pytest.mark.asyncio
+async def test_node_by_samba_fewer_trend_measurements(setup_samba_connection):
+    """Test getting a node's trends with a configured number of measurements."""
+    async with NodeSamba(TEST_NODE_IP_ADDRESS, TEST_NODE_PASSWORD) as node:
+        history = await node.async_get_history(measurements_to_use=3)
+
+    assert history["trends"] == {
+        "aqi_cn": "flat",
+        "aqi_us": "flat",
+        "co2": "decreasing",
+        "humidity": "decreasing",
+        "pm0_1": "flat",
+        "pm1_0": "decreasing",
+        "pm2_5": "flat",
+        "voc": "flat",
+    }
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "node_measurements_file", ["node_measurements_samba_list_response.json"]
+)
+async def test_node_by_samba_list_response(setup_samba_connection):
+    """Test getting a node's info over the local network (via Samba).
+
+    This variant of the test expects a list-esque response from the unit.
+    """
+    async with NodeSamba(TEST_NODE_IP_ADDRESS, TEST_NODE_PASSWORD) as node:
+        measurements = await node.async_get_latest_measurements()
+        history = await node.async_get_history()
+
+    assert len(history["measurements"]) == 7
+    assert measurements["last_measurement_timestamp"] == 1584204767
+    assert measurements["measurements"]["co2"] == "442"
+    assert measurements["measurements"]["humidity"] == "35"
+    assert measurements["measurements"]["pm0_1"] == "3"
+    assert measurements["measurements"]["pm1_0"] == "4"
+    assert measurements["measurements"]["aqi_cn"] == "6"
+    assert measurements["measurements"]["aqi_us"] == "17"
+    assert measurements["measurements"]["pm2_5"] == "4.0"
+    assert measurements["measurements"]["temperature_C"] == "19.3"
+    assert measurements["measurements"]["temperature_F"] == "66.8"
+    assert measurements["measurements"]["voc"] == "-1"
+
+    assert history["trends"] == {
+        "aqi_cn": "decreasing",
+        "aqi_us": "decreasing",
+        "co2": "decreasing",
+        "humidity": "increasing",
+        "pm0_1": "decreasing",
+        "pm1_0": "decreasing",
+        "pm2_5": "decreasing",
+        "voc": "flat",
+    }
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize(
+    "node_measurements_file", ["node_measurements_samba_no_sensor_life_response.json"]
+)
+async def test_node_by_samba_no_sensor_life_data(setup_samba_connection):
+    """Test a proper response when no sensor life values are returned."""
+    async with NodeSamba(TEST_NODE_IP_ADDRESS, TEST_NODE_PASSWORD) as node:
+        measurements = await node.async_get_latest_measurements()
+
+    assert measurements["status"]["sensor_life"] == {}
